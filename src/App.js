31df9127698c8b8951ece49794ea4d5002c06932
Scott Hirano <scottprotoss@gmail.com>
2022-11-05 21:28:15 -0400

    buggy commit, to be fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Box, Button, ButtonGroup, Flex, HStack, IconButton, Input, SkeletonText, Text } from '@chakra-ui/react'
 import { FaLocationArrow, FaTimes } from 'react-icons/fa'
-import { useJsApiLoader, GoogleMap, Marker, Autocomplete, DirectionsRenderer, Circle } from '@react-google-maps/api'
+import { useJsApiLoader, GoogleMap, Marker, Autocomplete, DirectionsRenderer, Circle, Polygon } from '@react-google-maps/api'
 import { useRef, useState } from 'react'
 // Starting position
 const center = { lat: 43.6607388, lng: -79.3988062 }
@@ -16,10 +16,14 @@ function App() {
   const [directionsResponse, setDirectionsResponse] = useState(null)
   const [markersList, setMarkersList] = useState([])
   const [circles, setCircles] = useState([])
+  const [coordinatesMaster, setCoordinatesMaster] = useState([])
+  const [searchResults, setSearchResults] = useState([])
+  const [searchMarkers, setSearchMarkers] = useState([])
   // Note: directionsResponse is recorded, but not rendered when we execute the calculateRoute().
   // Needs directionsRenderer is used to render route between destinations
   const [distance, setDistance] = useState('')
   const [duration, setDuration] = useState('')
+  const searchedMarker = []
 
   /** @type React.MutableRefObject<HTMLInputElement> */
   const originRef = useRef()
@@ -49,22 +53,26 @@ function App() {
     setDuration(results.routes[0].legs[0].duration.text)
     
     const leg = results.routes[0].legs[0]
-
+    const coordinates = []
     for(let j = 0; j < leg.steps.length; j++){
-      let skip = Math.ceil(leg.steps[j].path.length / 5)
+      if(leg.steps[j].distance.value < 1500) {continue};
+      let target = leg.steps[j].distance.value / 4000
+      let skip = Math.ceil(leg.steps[j].path.length / target)
       for(let i = 0; i < leg.steps[j].path.length; i+=skip){
         const coords = { 
           lat: leg.steps[j].path[i].lat(),
           lng: leg.steps[j].path[i].lng()
         }
+        //setCoordinatesMaster( (prev) => ([...prev, coords]))
+        coordinates.push(coords)
         setMarkersList( (prev) => ([...prev, <Marker position={coords}/>]))
-
-        
-        setCircles( (prev) => ([...prev, <Circle center={coords} radius ={5000} 
+        setCircles( (prev) => ([...prev, <Circle center={coords} radius ={8000} 
           options={ {strokeOpacity: 0.3, strokeWeight: 1, fillColor: '#FF0000', strokeColor: '#FF0000', fillOpacity: 0.3}}/>]));
         
       }
     }
+    setCoordinatesMaster(coordinates);
+    searching(coordinates);
   }
 
   // Clear Route when pressed X button
@@ -72,10 +80,52 @@ function App() {
     setDirectionsResponse(null)
     setDistance('')
     setDuration('')
+    
     originRef.current.value = ''
     destiantionRef.current.value = ''
   }
 
+  // *******************************************************************************
+  // *******************************************************************************
+  // Find nearby stuff
+  // *******************************************************************************
+  // *******************************************************************************
+  function searching(coordinates) {
+    console.log(coordinates)
+    const searchResultCoords =[];
+    const searchResultMarkers = [];
+    for(let currentCoord of coordinates) {
+      var request = {
+        query: 'Gas Station',
+        fields: ['name', 'geometry'],
+        locationBias: {radius: 8000, center: currentCoord}
+      };
+      // eslint-disable-next-line no-undef
+      let service = new google.maps.places.PlacesService(map)
+      console.log("entered")
+      service.findPlaceFromQuery(request, function(results, status) {
+        if(results !== null){
+          for (var i = 0; i < (results.length); i++) {
+            console.log(results[i].geometry.location)
+            searchResultCoords.push(results[i].geometry.location)
+            searchedMarker.push(<Marker position={
+              { 
+                lat: results[i].geometry.location.lat(),
+                lng: results[i].geometry.location.lng()
+              }}/>)
+            
+          }
+        }
+    
+        
+      })
+    };
+    
+    console.log(searchResultCoords)
+  }
+  function serachingQueries(){
+    //todo: iterate through all the checked off queries
+  };
   // *******************************************************************************
   // *******************************************************************************
   // Rendering
@@ -102,7 +152,8 @@ function App() {
           {directionsResponse && (
             <DirectionsRenderer directions={directionsResponse} />
           )}
-          <div>{markersList}{circles}</div>
+          <div>{coordinatesMaster}{searchedMarker}</div>
+          {/*add back {circles} after amagalating into one polygon */}
         </GoogleMap>
       </Box>
 
@@ -151,4 +202,4 @@ function App() {
     )
   }
   
-  export default App
+  export default App;
